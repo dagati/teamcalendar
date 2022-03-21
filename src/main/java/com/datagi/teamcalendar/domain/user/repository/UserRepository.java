e3dbcc9d2a6ee0589efb04b74714f89b87b2af56
pllap <pllapallpal@gmail.com>
2022-03-21 12:47:11 +0900

    Create UserRepository

---
+++ src/main/java/com/datagi/teamcalendar/domain/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.datagi.teamcalendar.domain.user.repository;
+
+import com.datagi.teamcalendar.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
