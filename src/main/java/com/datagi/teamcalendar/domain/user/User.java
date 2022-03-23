5b0b956593db2264b5c76a16e6a9179586ef8a10
pllap <pllapallpal@gmail.com>
2022-03-24 00:00:14 +0900

    Make User override `toString()`, `equals()`, `hashCode()`

--- src/main/java/com/datagi/teamcalendar/domain/user/User.java
+++ src/main/java/com/datagi/teamcalendar/domain/user/User.java
@@ -5,6 +5,7 @@ import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -33,4 +34,34 @@ public class User {
     @Enumerated(EnumType.STRING)
     @NotNull
     private Authority authority;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", authority=" + authority +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return Objects.equals(id, user.id) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(authority, user.authority);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.email, this.name, this.password, this.authority);
+    }
 }
