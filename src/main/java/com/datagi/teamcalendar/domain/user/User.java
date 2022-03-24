7280a7ea1eeb9da6d933732daf3c1f666d72b02b
pllap <pllapallpal@gmail.com>
2022-03-24 13:46:33 +0900

    Update User to use lombok `@EqualsAndHashCode` and `@ToString`

--- src/main/java/com/datagi/teamcalendar/domain/user/User.java
+++ src/main/java/com/datagi/teamcalendar/domain/user/User.java
@@ -5,12 +5,13 @@ import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.util.Objects;
 
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 @Builder
+@Getter
 @Entity
 public class User {
 
@@ -34,34 +35,4 @@ public class User {
     @Enumerated(EnumType.STRING)
     @NotNull
     private Authority authority;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        User user = (User) o;
-
-        return this.id.equals(user.id) &&
-                this.email.equals(user.email) &&
-                this.name.equals(user.name) &&
-                this.password.equals(user.password) &&
-                this.authority.equals(user.authority);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.id, this.email, this.name, this.password, this.authority);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                ", authority=" + authority +
-                '}';
-    }
 }
