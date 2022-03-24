635a94441b73fdb2ff02307575a5653bbb2179f0
pllap <pllapallpal@gmail.com>
2022-03-24 13:47:34 +0900

    Update UserTest

--- src/test/java/com/datagi/teamcalendar/user/UserTest.java
+++ src/test/java/com/datagi/teamcalendar/user/UserTest.java
@@ -3,14 +3,15 @@ package com.datagi.teamcalendar.user;
 import com.datagi.teamcalendar.domain.user.Authority;
 import com.datagi.teamcalendar.domain.user.User;
 import com.datagi.teamcalendar.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import javax.transaction.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Transactional
 @SpringBootTest
@@ -20,7 +21,8 @@ class UserTest {
     private UserRepository userRepository;
 
     @Test
-    void saveUserTest() {
+    @DisplayName("유저 저장 성공 테스트")
+    void saveUserSuccessTest() {
         User user = User.builder()
                 .email("alice@google.com")
                 .password("1q2w3e4r!!")
@@ -34,81 +36,28 @@ class UserTest {
         assertEquals(user.getId(), result.getId());
     }
 
-    /**
-     * equals 성공 테스트 (동일한 레퍼런스)
-     */
     @Test
-    void equalsSuccessTest1() {
-        User alice = User.builder()
-                .email("alice@google.com")
-                .password("1q2w3e4r!!")
-                .name("alice")
-                .authority(Authority.USER)
-                .build();
-        userRepository.save(alice);
+    @DisplayName("유저 저장 실패 테스트 (중복 이메일)")
+    void saveUserFailureTest() {
 
-        User bob = alice;
-
-        assertEquals(alice, bob);
-    }
-
-    /**
-     * equals 성공 테스트 (동일한 데이터)
-     */
-    @Test
-    void equalsSuccessTest2() {
-        String aliceEmail = "alice@google.com";
-
-        User alice = User.builder()
-                .email(aliceEmail)
-                .password("1q2w3e4r!!")
-                .name("alice")
-                .authority(Authority.USER)
-                .build();
-        userRepository.save(alice);
-
-        User aliceOnDB = userRepository.findByEmail(aliceEmail).orElse(null);
-
-        assertEquals(alice, aliceOnDB);
-    }
-
-    /**
-     * hashCode 테스트
-     */
-    @Test
-    void equalsHashCodeTest() {
-        User alice1 = User.builder()
-                .email("alice@google.com")
-                .password("1q2w3e4r!!")
-                .name("alice")
-                .authority(Authority.USER)
-                .build();
-        User alice2 = User.builder()
-                .email("alice@google.com")
-                .password("1q2w3e4r!!")
-                .name("alice")
-                .authority(Authority.USER)
-                .build();
-
-        assertEquals(alice1.hashCode(), alice2.hashCode());
-    }
-
-    /**
-     * toString 테스트
-     */
-    @Test
-    void toStringTest() {
+        String email = "duplicated@google.com";
 
         User user = User.builder()
-                .email("alice@google.com")
+                .email(email)
                 .password("1q2w3e4r!!")
                 .name("alice")
                 .authority(Authority.USER)
                 .build();
+        userRepository.save(user);
 
-        assertEquals(
-                "User{id=null, email='alice@google.com', name='alice', password='1q2w3e4r!!', authority=USER}",
-                user.toString()
-        );
+        assertThrows(DataIntegrityViolationException.class, () -> {
+            User anotherUser = User.builder()
+                    .email(email)
+                    .password("1q2w3e4r!!")
+                    .name("alice")
+                    .authority(Authority.USER)
+                    .build();
+            userRepository.save(anotherUser);
+        });
     }
 }
