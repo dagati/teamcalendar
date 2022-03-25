bbe4dfcdbad8c0bbbb5d845f274f133ae24a532e
unnokid <kimki0609@gmail.com>
2022-03-25 22:34:56 +0900

    Update AnnouncementTest

--- src/test/java/com/datagi/teamcalendar/announcement/AnnouncementTest.java
+++ src/test/java/com/datagi/teamcalendar/announcement/AnnouncementTest.java
@@ -5,13 +5,16 @@ import com.datagi.teamcalendar.domain.announcement.repository.AnnouncementReposi
 import com.datagi.teamcalendar.domain.user.Authority;
 import com.datagi.teamcalendar.domain.user.User;
 import com.datagi.teamcalendar.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
+import javax.validation.ConstraintViolationException;
 import java.time.LocalDateTime;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -19,13 +22,17 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SpringBootTest
 class AnnouncementTest {
 
-    @Autowired
-    private AnnouncementRepository announcementRepository;
+    private final AnnouncementRepository announcementRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    private UserRepository userRepository;
+    public AnnouncementTest(AnnouncementRepository announcementRepository, UserRepository userRepository) {
+        this.announcementRepository = announcementRepository;
+        this.userRepository = userRepository;
+    }
 
     @Test
+    @DisplayName("공지사항 저장 성공 테스트")
     void saveAnnouncementTest() {
 
         User user = User.builder()
@@ -47,6 +54,50 @@ class AnnouncementTest {
         Announcement result = announcementRepository.findById(announcement.getId()).orElse(null);
         assertNotNull(result);
         assertEquals(announcement.getId(), result.getId());
+    }
+
+    @Test
+    @DisplayName("공지사항 저장 실패 테스트(제목이 없음)")
+    void saveAnnouncementFailureTest() {
+
+        User user = User.builder()
+                .email("kim11@naver.com")
+                .name("ggo")
+                .password("1234")
+                .authority(Authority.ADMIN)
+                .build();
+        userRepository.save(user);
+
+        Announcement announcement = Announcement.builder()
+                .content("test content")
+                .createdDatetime(LocalDateTime.now())
+                .user(user)
+                .build();
+
+        assertThatThrownBy(() -> announcementRepository.save(announcement))
+                .isInstanceOf(ConstraintViolationException.class);
+    }
+
+    @Test
+    @DisplayName("공지사항 저장 실패 테스트(내용이 없음)")
+    void saveAnnouncementFailureTest2() {
+
+        User user = User.builder()
+                .email("kim11@naver.com")
+                .name("ggo")
+                .password("1234")
+                .authority(Authority.ADMIN)
+                .build();
+        userRepository.save(user);
+
+        Announcement announcement = Announcement.builder()
+                .title("hello friends")
+                .createdDatetime(LocalDateTime.now())
+                .user(user)
+                .build();
+
+        assertThatThrownBy(() -> announcementRepository.save(announcement))
+                .isInstanceOf(ConstraintViolationException.class);
     }
 
 }
