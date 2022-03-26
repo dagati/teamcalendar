04c624b5bc636b5a2c1a72cde6483b21b1d27de2
unnokid <kimki0609@gmail.com>
2022-03-26 23:33:39 +0900

    Create Participant

---
+++ src/main/java/com/datagi/teamcalendar/domain/participant/Participant.java
@@ -0,0 +1,30 @@
+package com.datagi.teamcalendar.domain.participant;
+
+import com.datagi.teamcalendar.domain.schedule.Schedule;
+import com.datagi.teamcalendar.domain.user.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
+@ToString
+@Entity
+public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name ="user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+}
