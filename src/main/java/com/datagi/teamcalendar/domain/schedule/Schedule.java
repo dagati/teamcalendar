fbbca452dd33077e646bf7d4b151efa5664dd5eb
unnokid <kimki0609@gmail.com>
2022-03-26 12:24:14 +0900

    Update Schedule

--- src/main/java/com/datagi/teamcalendar/domain/schedule/Schedule.java
+++ src/main/java/com/datagi/teamcalendar/domain/schedule/Schedule.java
@@ -8,7 +8,6 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
-
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -23,7 +22,7 @@ public class Schedule {
     private long id;
 
     @NotNull
-    @Length(max =40)
+    @Length(max = 40)
     private String name;
 
     @Length(max = 2000)
@@ -36,10 +35,10 @@ public class Schedule {
     private LocalDateTime endDatetime;
 
     @NotNull
-    @Length(max =10)
+    @Length(max = 10)
     private String color;
 
     @ManyToOne
-    @JoinColumn(name="team_id")
+    @JoinColumn(name = "team_id")
     private Team team;
 }
