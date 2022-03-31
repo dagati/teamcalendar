9e2b681050e8150347bbde1c65c662cd92d06644
pllap <pllapallpal@gmail.com>
2022-03-31 21:37:41 +0900

    Create RoleTest

---
+++ src/test/java/com/datagi/teamcalendar/role/RoleTest.java
@@ -0,0 +1,93 @@
+package com.datagi.teamcalendar.role;
+
+import com.datagi.teamcalendar.domain.role.Role;
+import com.datagi.teamcalendar.domain.role.repository.RoleRepository;
+import com.datagi.teamcalendar.domain.schedule.Schedule;
+import com.datagi.teamcalendar.domain.schedule.repository.ScheduleRepository;
+import com.datagi.teamcalendar.domain.team.Team;
+import com.datagi.teamcalendar.domain.team.repository.TeamRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import javax.validation.ConstraintViolationException;
+
+@SpringBootTest
+class RoleTest {
+    private final TeamRepository teamRepository;
+    private final ScheduleRepository scheduleRepository;
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RoleTest(
+            TeamRepository teamRepository,
+            ScheduleRepository scheduleRepository,
+            RoleRepository roleRepository
+    ) {
+        this.teamRepository = teamRepository;
+        this.scheduleRepository = scheduleRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    @Test
+    @DisplayName("역할 생성 성공 테스트")
+    void createRoleSuccessTest() {
+
+        Team team = teamRepository.findById(1L).orElse(null);
+        Schedule schedule = scheduleRepository.findById(1L).orElse(null);
+
+        Role role = Role.builder()
+                .name("professor")
+                .team(team)
+                .schedule(schedule)
+                .build();
+        roleRepository.save(role);
+
+        Assertions.assertThat(role.getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("역할 생성 실패 테스트 (팀 정보 없음)")
+    void createRoleFailureTest1() {
+        Schedule schedule = scheduleRepository.findById(1L).orElse(null);
+
+        Role role = Role.builder()
+                .name("professor")
+                .schedule(schedule)
+                .build();
+        Assertions.assertThatThrownBy(() -> roleRepository.save(role))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Test
+    @DisplayName("역할 생성 실패 테스트 (일정 정보 없음)")
+    void createRoleFailureTest2() {
+        Team team = teamRepository.getById(1L);
+
+        Role role = Role.builder()
+                .name("professor")
+                .team(team)
+                .build();
+
+        Assertions.assertThatThrownBy(() -> roleRepository.save(role))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Test
+    @DisplayName("역할 생성 실패 테스트 (역할명 누락)")
+    void createRoleFailureTest3() {
+        Team team = teamRepository.getById(1L);
+        Schedule schedule = scheduleRepository.getById(1L);
+
+        Role role = Role.builder()
+                .team(team)
+                .schedule(schedule)
+                .build();
+
+        Assertions.assertThatThrownBy(() -> roleRepository.save(role))
+                .isInstanceOf(ConstraintViolationException.class);
+    }
+}
