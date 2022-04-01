c80c82bf32850e44083a4c007c9004a869842b22
unnokid <kimki0609@gmail.com>
2022-04-01 23:16:41 +0900

    Update PariticipantTest using the repository

--- src/test/java/com/datagi/teamcalendar/participant/ParticipantTest.java
+++ src/test/java/com/datagi/teamcalendar/participant/ParticipantTest.java
@@ -4,9 +4,6 @@ import com.datagi.teamcalendar.domain.participant.Participant;
 import com.datagi.teamcalendar.domain.participant.repository.ParticipantRepository;
 import com.datagi.teamcalendar.domain.schedule.Schedule;
 import com.datagi.teamcalendar.domain.schedule.repository.ScheduleRepository;
-import com.datagi.teamcalendar.domain.team.Team;
-import com.datagi.teamcalendar.domain.team.repository.TeamRepository;
-import com.datagi.teamcalendar.domain.user.Authority;
 import com.datagi.teamcalendar.domain.user.User;
 import com.datagi.teamcalendar.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -16,7 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
 
 import javax.transaction.Transactional;
-import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -26,18 +22,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ParticipantTest {
 
     private final UserRepository userRepository;
-    private final TeamRepository teamRepository;
     private final ScheduleRepository scheduleRepository;
     private final ParticipantRepository participantRepository;
 
     @Autowired
-    public ParticipantTest(
-            UserRepository userRepository,
-            TeamRepository teamRepository,
-            ScheduleRepository scheduleRepository,
-            ParticipantRepository participantRepository) {
+    public ParticipantTest(UserRepository userRepository,
+                           ScheduleRepository scheduleRepository,
+                           ParticipantRepository participantRepository) {
         this.userRepository = userRepository;
-        this.teamRepository = teamRepository;
         this.scheduleRepository = scheduleRepository;
         this.participantRepository = participantRepository;
     }
@@ -46,8 +38,8 @@ class ParticipantTest {
     @DisplayName("참가자 저장 성공 테스트")
     void saveParticipantSuccessTest() {
 
-        User user = makeUser();
-        Schedule schedule = makeSchedule();
+        User user = userRepository.getById(1L);
+        Schedule schedule = scheduleRepository.getById(1L);
 
         Participant participant = Participant.builder()
                 .user(user)
@@ -65,7 +57,7 @@ class ParticipantTest {
     @DisplayName("참가자 저장 실패 테스트(유저가 없음)")
     void saveParticipantFailureTest() {
 
-        Schedule schedule = makeSchedule();
+        Schedule schedule = scheduleRepository.getById(1L);
 
         Participant participant = Participant.builder()
                 .schedule(schedule)
@@ -79,7 +71,7 @@ class ParticipantTest {
     @DisplayName("참가자 저장 실패 테스트(일정이 없음)")
     void saveParticipantFailureTest2() {
 
-        User user = makeUser();
+        User user = userRepository.getById(1L);
 
         Participant participant = Participant.builder()
                 .user(user)
@@ -87,44 +79,5 @@ class ParticipantTest {
 
         assertThatThrownBy(() -> participantRepository.save(participant))
                 .isInstanceOf(DataIntegrityViolationException.class);
-    }
-
-    private Schedule makeSchedule() {
-        User user = User.builder()
-                .email("kim@gmail.com")
-                .password("kim123")
-                .name("Champon")
-                .authority(Authority.USER)
-                .build();
-        userRepository.save(user);
-
-        Team team = Team.builder()
-                .name("kick")
-                .leader(user)
-                .build();
-        teamRepository.save(team);
-
-        Schedule schedule = Schedule.builder()
-                .name("Grilled Fish")
-                .detail("go to eat Grilled Fish")
-                .startDatetime(LocalDateTime.of(2022, 5, 3, 12, 0))
-                .endDatetime(LocalDateTime.of(2022, 5, 3, 13, 0))
-                .color("#000000")
-                .team(team)
-                .build();
-        scheduleRepository.save(schedule);
-        return schedule;
-    }
-
-    private User makeUser() {
-        User user = User.builder()
-                .email("kkk3@gmail.com")
-                .password("kkk123")
-                .name("cocoball")
-                .authority(Authority.USER)
-                .build();
-        userRepository.save(user);
-
-        return user;
     }
 }
