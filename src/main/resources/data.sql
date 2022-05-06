96b57275f3c9a6227bec976ef9112e3b30c33708
pllap <pllapallpal@gmail.com>
2022-05-06 22:52:41 +0900

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,73 +1,33 @@
 -- user
-INSERT INTO `user`(`id`, `email`, `name`, `password`, `authority`)
-VALUES (null, 'alice@gmail.com', 'alice', 'alice', 'ADMIN');
-INSERT INTO `user`(`id`, `email`, `name`, `password`, `authority`)
-VALUES (null, 'bob@gmail.com', 'bob', 'bob', 'ADMIN');
-INSERT INTO `user`(`id`, `email`, `name`, `password`, `authority`)
-VALUES (null, 'charlie@gmail.com', 'charlie', 'charlie', 'USER');
-INSERT INTO `user`(`id`, `email`, `name`, `password`, `authority`)
-VALUES (null, 'david@gmail.com', 'david', 'david', 'USER');
-INSERT INTO `user`(`id`, `email`, `name`, `password`, `authority`)
-VALUES (null, 'eve@gmail.com', 'eve', 'eve', 'USER');
-INSERT INTO `user`(`id`, `email`, `name`, `password`, `authority`)
-VALUES (null, 'faythe@gmail.com', 'faythe', 'faythe', 'USER');
-INSERT INTO `user`(`id`, `email`, `name`, `password`, `authority`)
-VALUES (null, 'grace@gmail.com', 'grace', 'grace', 'USER');
-INSERT INTO `user`(`id`, `email`, `name`, `password`, `authority`)
-VALUES (null, 'heidi@gmail.com', 'heidi', 'heidi', 'USER');
-INSERT INTO `user`(`id`, `email`, `name`, `password`, `authority`)
-VALUES (null, 'ivan@gmail.com', 'ivan', 'ivan', 'USER');
-INSERT INTO `user`(`id`, `email`, `name`, `password`, `authority`)
-VALUES (null, 'trudy@gmail.com', 'trudy', 'trudy', 'ADMIN');
+INSERT INTO `user`(`id`, `name`, `email`, `password`, `authority`)
+VALUES (null, 'alice', 'alice@gmail.com', 'alicepw', 'ADMIN');
+INSERT INTO `user`(`id`, `name`, `email`, `password`, `authority`)
+VALUES (null, 'bob', 'bob@gmail.com', 'bobpw', 'USER');
 
 -- team
-INSERT INTO `team`(`id`, `name`, `leader_id`)
-VALUES (null, 'alpha', 1);
-INSERT INTO `team`(`id`, `name`, `leader_id`)
-VALUES (null, 'bravo', 2);
-INSERT INTO `team`(`id`, `name`, `leader_id`)
-VALUES (null, 'charlie', 3);
+INSERT INTO `team`(`id`, `name`, `code`)
+VALUES (null, 'alpha', '1111111111111111');
 
--- invitation_code
-INSERT INTO `invitation_code`(`id`, `token`, `team_id`)
-VALUES (null, '1111111111111111', 1);
-INSERT INTO `invitation_code`(`id`, `token`, `team_id`)
-VALUES (null, '2222222222222222', 2);
-INSERT INTO `invitation_code`(`id`, `token`, `team_id`)
-VALUES (null, '3333333333333333', 3);
-
--- member_list
-INSERT INTO `member_list`(`id`, `team_id`, `user_id`)
+-- member
+INSERT INTO `member`(`id`, `team_id`, `user_id`)
 VALUES (null, 1, 1);
-INSERT INTO `member_list`(`id`, `team_id`, `user_id`)
-VALUES (null, 1, 4);
-INSERT INTO `member_list`(`id`, `team_id`, `user_id`)
-VALUES (null, 1, 5);
-INSERT INTO `member_list`(`id`, `team_id`, `user_id`)
-VALUES (null, 2, 2);
-INSERT INTO `member_list`(`id`, `team_id`, `user_id`)
-VALUES (null, 2, 6);
-INSERT INTO `member_list`(`id`, `team_id`, `user_id`)
-VALUES (null, 2, 7);
-INSERT INTO `member_list`(`id`, `team_id`, `user_id`)
-VALUES (null, 3, 3);
-INSERT INTO `member_list`(`id`, `team_id`, `user_id`)
-VALUES (null, 3, 8);
-INSERT INTO `member_list`(`id`, `team_id`, `user_id`)
-VALUES (null, 3, 9);
+
+-- role
+INSERT INTO `role`(`id`, `name`, `team_id`)
+VALUES (null, 'leader', 1);
+
+-- position
+INSERT INTO `position`(`id`, `role_id`, `member_id`)
+VALUES (null, 1, 1);
+
+-- schedule
+INSERT INTO `schedule`(`id`, `name`, `detail`, `begin_date`, `begin_time`, `end_date`, `end_time`, `color`, `team_id`)
+VALUES (null, '통합 회의', '통합 회의', '2022-06-15', '09:00:00', '2022-06-15', '10:00:00', '#907090', 1);
+
+-- participant
+INSERT INTO `participant`(`id`, `schedule_id`, `member_id`)
+VALUES (null, 1, 1);
 
 -- announcement
 INSERT INTO `announcement`(`id`, `title`, `content`, `created_datetime`, `writer_id`)
-VALUES (null, 'announcement 1', '공식 사이트를 보면, Maven은 소프트웨어 프로젝트 관리 및 생성 도구라고 정의하고 있다. 즉, Maven은 라이브러리를 관리하고 프로젝트를 빌드하는 도구이다. 그런데 왜 굳이 Maven을 사용해서 라이브러리를 관리해야 할까?', '2022-03-01 09:00:00', 1);
-INSERT INTO `announcement`(`id`, `title`, `content`, `created_datetime`, `writer_id`)
-VALUES (null, 'announcement 2', '사용자가 입력한 URL 주소 중 도메인 네임을 DNS 서버에 검색하고 DNS 서버에서 해당 도메인 네임에 해당하는 IP 주소를 찾아 사용자가 입력한 URL 정보와 함께 전달한다.', '2022-03-02 12:00:00', 2);
-INSERT INTO `announcement`(`id`, `title`, `content`, `created_datetime`, `writer_id`)
-VALUES (null, 'announcement 3', 'HTTP(Hyper Text Transfer Protocol)로 서버/클라이언트 모델을 따라 데이터를 주고받기 위한 프로토콜이다. 80번 포트를 사용하며 HTTP 서버가 80번 포트에서 요청을 기다리고 있으며 클라이언트는 80번 포트로 요청을 보내게 된다.', '2022-03-03 15:00:00', 10);
-
--- schedule
-INSERT INTO `schedule`(`id`, `name`, `detail`, `start_datetime`, `end_datetime`, `color`, `team_id`)
-VALUES (null, '통합 회의', '통합 회의', '2022-03-14 09:00:00', '2022-03-14 10:00:00', '#907090', 1);
-
--- role
-INSERT INTO `role`(`id`, `name`, `team_id`,`schedule_id`)
-VALUES (null, '백엔드', 1, 1);
+VALUES (null, '안녕하세요. 팀캘린더입니다.', '팀캘린더는 팀 단위로 일정을 관리하는 서비스입니다.', '2022-06-01 09:00:00', 1);
