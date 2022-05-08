f74aa367cb2f59f1534f51887c80485c26b8b1a9
pllap <pllapallpal@gmail.com>
2022-05-08 22:05:03 +0900

    Update TeamTest

--- src/test/java/com/datagi/teamcalendar/team/TeamTest.java
+++ src/test/java/com/datagi/teamcalendar/team/TeamTest.java
@@ -2,37 +2,67 @@ package com.datagi.teamcalendar.team;
 
 import com.datagi.teamcalendar.domain.team.Team;
 import com.datagi.teamcalendar.domain.team.repository.TeamRepository;
-import com.datagi.teamcalendar.domain.user.User;
-import com.datagi.teamcalendar.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.transaction.Transactional;
-import javax.validation.ConstraintViolationException;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@Transactional
+@ActiveProfiles("develop")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 class TeamTest {
 
-    private final UserRepository userRepository;
     private final TeamRepository teamRepository;
 
     @Autowired
-    public TeamTest(UserRepository userRepository, TeamRepository teamRepository) {
-        this.userRepository = userRepository;
+    public TeamTest(TeamRepository teamRepository) {
         this.teamRepository = teamRepository;
+    }
+
+    @Test
+    @DisplayName("팀 저장 실패 테스트 (팀 이름 누락)")
+    void saveTeamFailureTest1() {
+
+        Team team = Team.builder()
+                .build();
+
+        assertThatThrownBy(() -> teamRepository.save(team))
+                .isInstanceOf(Exception.class);
+    }
+
+    @Test
+    @DisplayName("팀 저장 실패 테스트 (너무 긴 팀 이름)")
+    void saveTeamFailureTest2() {
+
+        Team team = Team.builder()
+                .name("a verrrrrrrrrrrrrrrry long team name")
+                .build();
+
+        assertThatThrownBy(() -> teamRepository.save(team))
+                .isInstanceOf(Exception.class);
+    }
+
+    @Test
+    @DisplayName("팀 저장 실패 테스트 (길이가 다른 입장 코드)")
+    void saveTeamFailureTest3() {
+
+        Team team = Team.builder()
+                .name("dagati")
+                .code("123456789012345678901234567890")
+                .build();
+
+        assertThatThrownBy(() -> teamRepository.save(team))
+                .isInstanceOf(Exception.class);
     }
 
     @Test
     @DisplayName("팀 저장 성공 테스트")
     void saveTeamSuccessTest() {
-
-        User leader = userRepository.getById(1L);
 
         Team team = Team.builder()
                 .name("dagati")
@@ -40,18 +70,5 @@ class TeamTest {
         teamRepository.save(team);
 
         assertThat(team.getId()).isNotNull();
-    }
-
-    @Test
-    @DisplayName("팀 저장 실패 테스트 (팀 이름이 없음)")
-    void saveTeamFailureTest() {
-
-        User leader = userRepository.getById(1L);
-
-        Team team = Team.builder()
-                .build();
-
-        assertThatThrownBy(() -> teamRepository.save(team))
-                .isInstanceOf(ConstraintViolationException.class);
     }
 }
