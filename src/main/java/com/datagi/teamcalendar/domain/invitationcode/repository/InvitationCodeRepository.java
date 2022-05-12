9d87a53f16a4e86dba3efafd1e51baf8d67033d0
pllap <pllapallpal@gmail.com>
2022-05-12 23:53:54 +0900

    Delete entities no longer exist in design

--- src/main/java/com/datagi/teamcalendar/domain/invitationcode/repository/InvitationCodeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.datagi.teamcalendar.domain.invitationcode.repository;
-
-import com.datagi.teamcalendar.domain.invitationcode.InvitationCode;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvitationCodeRepository extends JpaRepository<InvitationCode, Long> {
-}
--- src/main/java/com/datagi/teamcalendar/domain/rolelist/repository/RoleListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.datagi.teamcalendar.domain.rolelist.repository;
-
-import com.datagi.teamcalendar.domain.rolelist.RoleList;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleListRepository extends JpaRepository<RoleList,Long> {
-}
