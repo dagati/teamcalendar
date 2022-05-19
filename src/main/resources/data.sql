a10815eadf8dfaa4ae3f10d5f7c27e1daf2a352d
pllap <pllapallpal@gmail.com>
2022-05-20 01:38:19 +0900

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,10 +3,18 @@ INSERT INTO `user`(`id`, `name`, `email`, `password`, `authority`)
 VALUES (null, 'alice', 'alice@gmail.com', 'alicepw', 'ADMIN');
 INSERT INTO `user`(`id`, `name`, `email`, `password`, `authority`)
 VALUES (null, 'bob', 'bob@gmail.com', 'bobpw', 'USER');
+INSERT INTO `user`(`id`, `name`, `email`, `password`, `authority`)
+VALUES (null, 'charlie', 'charlie@gmail.com', 'charliepw', 'USER');
 
 -- team
 INSERT INTO `team`(`id`, `name`, `code`)
 VALUES (null, 'alpha', '1111111111111111');
+INSERT INTO `team`(`id`, `name`, `code`)
+VALUES (null, 'beta', '2222222222222222');
+
+-- team_relation
+INSERT INTO `team_relation`(`id`, `parent_id`, `child_id`, `depth`)
+VALUES (null, 1, 1, 0);
 
 -- member
 INSERT INTO `member`(`id`, `team_id`, `user_id`)
@@ -28,6 +36,6 @@ VALUES (null, '통합 회의', '통합 회의', '2022-06-15', '09:00:00', '2022-
 INSERT INTO `participant`(`id`, `schedule_id`, `member_id`)
 VALUES (null, 1, 1);
 
--- announcement
-INSERT INTO `announcement`(`id`, `title`, `content`, `created_datetime`, `writer_id`)
+-- notice
+INSERT INTO `notice`(`id`, `title`, `content`, `created_datetime`, `writer_id`)
 VALUES (null, '안녕하세요. 팀캘린더입니다.', '팀캘린더는 팀 단위로 일정을 관리하는 서비스입니다.', '2022-06-01 09:00:00', 1);
