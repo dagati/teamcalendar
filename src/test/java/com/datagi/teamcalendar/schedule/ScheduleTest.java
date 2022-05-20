9382c046e78296c89a7309591ec8f824da4c0e86
pllap <pllapallpal@gmail.com>
2022-05-20 23:29:45 +0900

    Update ScheduleTest

--- src/test/java/com/datagi/teamcalendar/schedule/ScheduleTest.java
+++ src/test/java/com/datagi/teamcalendar/schedule/ScheduleTest.java
@@ -6,17 +6,20 @@ import com.datagi.teamcalendar.domain.team.Team;
 import com.datagi.teamcalendar.domain.team.repository.TeamRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-import javax.transaction.Transactional;
 import javax.validation.ConstraintViolationException;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@Transactional
+@ActiveProfiles("develop")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 class ScheduleTest {
 
@@ -24,43 +27,26 @@ class ScheduleTest {
     private final ScheduleRepository scheduleRepository;
 
     @Autowired
-    public ScheduleTest(TeamRepository teamRepository,
-                        ScheduleRepository scheduleRepository
+    public ScheduleTest(
+            TeamRepository teamRepository,
+            ScheduleRepository scheduleRepository
     ) {
         this.teamRepository = teamRepository;
         this.scheduleRepository = scheduleRepository;
     }
 
     @Test
-    @DisplayName("일정 저장 성공 테스트")
-    void saveScheduleSuccessTest() {
-
-        Team team = teamRepository.getById(1L);
-
-        Schedule schedule = Schedule.builder()
-                .name("Grilled Fish")
-                .detail("go to eat Grilled Fish")
-                .startDatetime(LocalDateTime.of(2022, 5, 3, 12, 0))
-                .endDatetime(LocalDateTime.of(2022, 5, 3, 13, 0))
-                .color("#000000")
-                .team(team)
-                .build();
-
-        scheduleRepository.save(schedule);
-
-        assertThat(schedule.getId()).isNotNull();
-    }
-
-    @Test
-    @DisplayName("일정 저장 실패 테스트 (일정 제목이 없음)")
+    @DisplayName("일정 저장 실패 테스트 (일정 제목 누락)")
     void saveScheduleFailureTest1() {
 
         Team team = teamRepository.getById(1L);
 
         Schedule schedule = Schedule.builder()
                 .detail("go to eat Grilled Fish")
-                .startDatetime(LocalDateTime.of(2022, 5, 3, 12, 0))
-                .endDatetime(LocalDateTime.of(2022, 5, 3, 13, 0))
+                .beginDate(LocalDate.of(2022, 5, 15))
+                .beginTime(LocalTime.of(12, 0))
+                .endDate(LocalDate.of(2022, 5, 15))
+                .endTime(LocalTime.of(13, 0))
                 .color("#000000")
                 .team(team)
                 .build();
@@ -78,13 +64,77 @@ class ScheduleTest {
         Schedule schedule = Schedule.builder()
                 .name("I like Fish and Milk so let's go~~ and I am happy")
                 .detail("go to eat Grilled Fish")
-                .startDatetime(LocalDateTime.of(2022, 5, 3, 12, 0))
-                .endDatetime(LocalDateTime.of(2022, 5, 3, 13, 0))
+                .beginDate(LocalDate.of(2022, 5, 15))
+                .beginTime(LocalTime.of(12, 0))
+                .endDate(LocalDate.of(2022, 5, 15))
+                .endTime(LocalTime.of(13, 0))
                 .color("#000000")
                 .team(team)
                 .build();
 
         assertThatThrownBy(() -> scheduleRepository.save(schedule))
                 .isInstanceOf(ConstraintViolationException.class);
+    }
+
+    @Test
+    @DisplayName("일정 저장 실패 테스트 (시작 날짜가 끝 날짜보다 앞섬)")
+    void saveScheduleFailureTest3() {
+
+        Team team = teamRepository.getById(1L);
+
+        Schedule.ScheduleBuilder scheduleBuilder = Schedule.builder()
+                .name("Grilled Fish")
+                .detail("go to eat Grilled Fish")
+                .beginDate(LocalDate.of(2022, 5, 15))
+                .beginTime(LocalTime.of(12, 0))
+                .endDate(LocalDate.of(2022, 5, 14))
+                .endTime(LocalTime.of(13, 0))
+                .color("#000000")
+                .team(team);
+
+        assertThatThrownBy(scheduleBuilder::build)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("일정 저장 실패 테스트 (시작 시간이 끝 시간보다 앞섬)")
+    void saveScheduleFailureTest4() {
+
+        Team team = teamRepository.getById(1L);
+
+        Schedule.ScheduleBuilder scheduleBuilder = Schedule.builder()
+                .name("Grilled Fish")
+                .detail("go to eat Grilled Fish")
+                .beginDate(LocalDate.of(2022, 5, 15))
+                .beginTime(LocalTime.of(12, 0))
+                .endDate(LocalDate.of(2022, 5, 15))
+                .endTime(LocalTime.of(10, 0))
+                .color("#000000")
+                .team(team);
+
+        assertThatThrownBy(scheduleBuilder::build)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("일정 저장 성공 테스트")
+    void saveScheduleSuccessTest() {
+
+        Team team = teamRepository.getById(1L);
+
+        Schedule schedule = Schedule.builder()
+                .name("Grilled Fish")
+                .detail("go to eat Grilled Fish")
+                .beginDate(LocalDate.of(2022, 5, 15))
+                .beginTime(LocalTime.of(12, 0))
+                .endDate(LocalDate.of(2022, 5, 15))
+                .endTime(LocalTime.of(13, 0))
+                .color("#000000")
+                .team(team)
+                .build();
+
+        scheduleRepository.save(schedule);
+
+        assertThat(schedule.getId()).isNotNull();
     }
 }
