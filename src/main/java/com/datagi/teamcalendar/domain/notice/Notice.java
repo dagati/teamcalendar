9bad5c583d2166bdbf6abbcd1de441d508ec6530
pllap <pllapallpal@gmail.com>
2022-05-21 09:11:47 +0900

    Update Notice

--- src/main/java/com/datagi/teamcalendar/domain/notice/Notice.java
+++ src/main/java/com/datagi/teamcalendar/domain/notice/Notice.java
@@ -3,10 +3,11 @@ package com.datagi.teamcalendar.domain.notice;
 import com.datagi.teamcalendar.domain.user.User;
 import com.datagi.teamcalendar.global.entity.BaseTimeEntity;
 import lombok.*;
-import org.hibernate.validator.constraints.Length;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Getter
@@ -23,7 +24,7 @@ public class Notice extends BaseTimeEntity {
     private Long id;
 
     @NotNull
-    @Length(max = 100)
+    @Size(max = 100)
     private String title;
 
     @NotNull
@@ -31,9 +32,11 @@ public class Notice extends BaseTimeEntity {
     private String content;
 
     @NotNull
+    @CreatedDate
     private LocalDateTime createdDatetime;
 
+    @NotNull
     @ManyToOne
-    @JoinColumn(name="writer_id")
+    @JoinColumn(name = "writer_id")
     private User user;
 }
